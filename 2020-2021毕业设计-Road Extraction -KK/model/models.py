c83edc7d4e11169d1ed031b529a9144ce854b9e1
klein <35882820+kangkau@users.noreply.github.com>
2021-05-03 23:50:30 +0800

    Delete 2020-2021毕业设计-Road Extraction -KK directory

--- 2020-2021毕业设计-Road Extraction -KK/model/models.py
+++
@@ -1,7 +0,0 @@
-from model.linknet import LinkNet34, LinkNet34MTL
-from model.stack_module import StackHourglassNetMTL
-
-
-MODELS = {"LinkNet34MTL": LinkNet34MTL, "StackHourglassNetMTL": StackHourglassNetMTL}
-
-MODELS_REFINE = {"LinkNet34": LinkNet34}
